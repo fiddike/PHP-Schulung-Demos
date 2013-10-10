17e7da80f7a77cf861ad866e5b69fcc5b52c14d0
Timon Schroeter <timon.schroeter@marketvalid.com>
2013-10-10 14:20:50 +0200

    Simple session example

--- web/Symfony-2.3/src/PhpSchulungDe/SessionDemoBundle/Controller/DefaultController.php
+++ web/Symfony-2.3/src/PhpSchulungDe/SessionDemoBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace PhpSchulungDe\SessionDemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,33 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/session-counter-up/")
+     * @Template()
+     */
+    public function sessionCounterUpAction(Request $request)
+    {
+        $session = $request->getSession();
+        $demo_counter = $session->get('demo_counter');
+        if (!isset($demo_counter)) {
+            $demo_counter = 0;
+        }
+        $session->set('demo_counter', ++$demo_counter);
+
+        return array('demo_counter' => $demo_counter);
+    }
+
+    /**
+     * @Route("/session-counter-reset/")
+     * @Template("PhpSchulungDeSessionDemoBundle:Default:sessionCounterUp.html.twig")
+     */
+    public function sessionCounterResetAction(Request $request)
+    {
+        $session = $request->getSession();
+        $session->set('demo_counter', $demo_counter = 0);
+
+        return array('demo_counter' => $demo_counter);
     }
 }
