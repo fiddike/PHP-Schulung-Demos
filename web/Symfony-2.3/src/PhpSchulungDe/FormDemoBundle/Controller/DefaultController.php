4440da79e06512bff7d66fd95db89ceb8d7f3c76
Timon Schroeter <timon.schroeter@marketvalid.com>
2013-10-10 15:14:39 +0200

    Simple demo of grabbing post data in clean and unclean ways

--- web/Symfony-2.3/src/PhpSchulungDe/FormDemoBundle/Controller/DefaultController.php
+++ web/Symfony-2.3/src/PhpSchulungDe/FormDemoBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace PhpSchulungDe\FormDemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,5 +17,39 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/post-demo-form/", name="post_demo_form")
+     * @Template()
+     */
+    public function postDemoFormAction()
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('post_demo_show'))
+            ->add('fooText', 'text')
+            ->add('save', 'submit')
+            ->getForm();
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * @Route("/post-demo-show/", name="post_demo_show")
+     */
+    public function postDemoShowAction(Request $request)
+    {
+        // echo '$_POST :      ';
+        // var_dump($_POST);
+
+        // echo '$request->request :      ';
+        // var_dump($request->request);
+
+        $formData = $request->get('form');
+        echo $formData['fooText'];
+
+        return new Response('');
     }
 }
