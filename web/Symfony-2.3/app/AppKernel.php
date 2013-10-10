1f6fb06c66b2ee43a5155a00b014636f323adf6d
Timon Schroeter <timon.schroeter@marketvalid.com>
2013-10-10 14:31:14 +0200

    generated bundle

--- web/Symfony-2.3/app/AppKernel.php
+++ web/Symfony-2.3/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PhpSchulungDe\SessionDemoBundle\PhpSchulungDeSessionDemoBundle(),
+            new PhpSchulungDe\FormDemoBundle\PhpSchulungDeFormDemoBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
