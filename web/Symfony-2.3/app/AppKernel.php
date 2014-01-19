d9217b6ae4452cf0faf6563ce64936fadd264d3c
Timon Schroeter <timon.schroeter@marketvalid.com>
2014-01-19 19:44:14 +0100

    Add config debug command

--- web/Symfony-2.3/app/AppKernel.php
+++ web/Symfony-2.3/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new PhpSchulungDe\SessionDemoBundle\PhpSchulungDeSessionDemoBundle(),
             new PhpSchulungDe\FormDemoBundle\PhpSchulungDeFormDemoBundle(),
             new PhpSchulungDe\ConfigDemoBundle\PhpSchulungDeConfigDemoBundle(),
+            new Egulias\ListenersDebugCommandBundle\EguliasListenersDebugCommandBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
