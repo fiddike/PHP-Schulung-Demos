d8315c892ed8aa97632080224d1af9dc787c6add
Timon Fiddike <timon.fiddike@marketvalid.com>
2014-12-06 11:46:48 +0100

    debug demo

--- web/Symfony-2.6/src/PhpSchulungDe/DemoBundle/Controller/DefaultController.php
+++ web/Symfony-2.6/src/PhpSchulungDe/DemoBundle/Controller/DefaultController.php
@@ -5,18 +5,49 @@ namespace PhpSchulungDe\DemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+/**
+ * @Route("/psd")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/psd/hello/{name}")
+     * @Route("/exception")
+     */
+    public function exceptionAction()
+    {
+        throw new NotFoundHttpException ('This Exception is thrown to simulate that content has not been found.');
+    }
+
+    /**
+     * @Route("/dump")
      * @Template()
      */
-    public function indexAction($name)
+    public function dumpAction()
     {
         $foo = array('a', 'b', 'c');
         dump($foo);
 
-        return array('name' => $name);
+        return array();
+    }
+
+    /**
+     * @Route("/debug")
+     */
+    public function debugMeAction()
+    {
+        $foo = array('a', 'b', 'c');
+
+        $i = 1;
+        $k = 2;
+
+        $i = 10;
+        $k = 20;
+
+        $p = 100;
+        $p = 200;
+
+        throw new NotFoundHttpException ('Debug me, please!');
     }
 }
